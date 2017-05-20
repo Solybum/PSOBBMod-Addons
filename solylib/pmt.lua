56ba79de69337f0955f16369d260cdb9541f1304
Soleil Rojas <soleilrojas@outlook.com>
2017-05-20 09:10:04 -0600

    Added armor pmt data reading

--- solylib/pmt.lua
+++ solylib/pmt.lua
@@ -5,64 +5,72 @@ local pmtUnitOffset = 0x08
 local pmtMagOffset = 0x10
 local pmtToolOffset = 0x0C
 
+local pmtAddress = 0
+
+local function LoadPMTAddress()
+    pmtAddress = pso.read_u32(pmtPointer)
+    return pmtAddress ~= 0
+end
+
+-- Easy UnitxtID lookup
 local function _GetItemUnitxtID(type, group, index)
     local id = -1
 
-    local address = pso.read_u32(pmtPointer)
-    if address ~= 0 then
+    if LoadPMTAddress() ~= 0 then
         if type == 0 then
-            address = pso.read_u32(address + pmtWeaponOffset)
-            if address ~= 0 then
-                local groupAddress = address + 8 * group
+            local weaponAddress = pso.read_u32(pmtAddress + pmtWeaponOffset)
+            if weaponAddress ~= 0 then
+                local groupAddress = weaponAddress + 8 * group
 
                 local count = pso.read_u32(groupAddress)
-                address = pso.read_u32(groupAddress + 4)
+                local itemAddress = pso.read_u32(groupAddress + 4)
 
-                if index < count and address ~= 0 then
-                    id = pso.read_i32(address + 44 * index)
+                if index < count and itemAddress ~= 0 then
+                    id = pso.read_i32(itemAddress + 44 * index)
                 end
             end
         elseif type == 1 then
             if group == 1 or group == 2 then
-                address = pso.read_u32(address + pmtArmorOffset)
-                if address ~= 0 then
-                    address = address + 8 * (group - 1)
+                local armorAddress = pso.read_u32(pmtAddress + pmtArmorOffset)
+                if armorAddress ~= 0 then
+                    local groupAddress = armorAddress + 8 * (group - 1)
 
-                    local count = pso.read_u32(address)
-                    address = pso.read_u32(address + 4)
+                    local count = pso.read_u32(groupAddress)
+                    local itemAddress = pso.read_u32(groupAddress + 4)
 
-                    if index< count and address ~= 0 then
-                        id = pso.read_i32(address + 32 * index)
+                    if index < count and itemAddress ~= 0 then
+                        id = pso.read_i32(itemAddress + 32 * index)
                     end
                 end
             elseif group == 3 then
-                address = pso.read_u32(address + pmtUnitOffset)
+                local unitAddress = pso.read_u32(pmtAddress + pmtUnitOffset)
 
-                local count = pso.read_u32(address)
-                address = pso.read_u32(address + 4)
+                local count = pso.read_u32(unitAddress)
+                local itemAddress = pso.read_u32(unitAddress + 4)
 
-                if index < count and address ~= 0 then
-                    id = pso.read_i32(address + 20 * index)
+                if index < count and itemAddress ~= 0 then
+                    id = pso.read_i32(itemAddress + 20 * index)
                 end
             end
         elseif type == 2 then
-            address = pso.read_u32(address + pmtMagOffset)
+            local magAddress = pso.read_u32(pmtAddress + pmtMagOffset)
 
-            local count = pso.read_u32(address)
-            address = pso.read_u32(address + 4)
-            if group < count and address ~= 0 then
-                id = pso.read_i32(address + 28 * group)
+            local count = pso.read_u32(magAddress)
+            local itemAddress = pso.read_u32(magAddress + 4)
+
+            if group < count and itemAddress ~= 0 then
+                id = pso.read_i32(itemAddress + 28 * group)
             end
         elseif type == 3 then
-            address = pso.read_u32(address + pmtToolOffset)
-            if address ~= 0 then
-                local groupAddress = address + 8 * group
+            local toolAddress = pso.read_u32(pmtAddress + pmtToolOffset)
+            if toolAddress ~= 0 then
+                local groupAddress = toolAddress + 8 * group
 
                 local count = pso.read_u32(groupAddress)
-                address = pso.read_u32(groupAddress + 4)
+                local itemAddress = pso.read_u32(groupAddress + 4)
 
-                if index < count and address ~= 0 then
-                    id = pso.read_i32(address + 24 * index)
+                if index < count and itemAddress ~= 0 then
+                    id = pso.read_i32(itemAddress + 24 * index)
                 end
             end
         end
@@ -70,12 +78,139 @@ local function _GetItemUnitxtID(type, group, index)
 
     return id
 end
-
 local function GetItemUnitxtID(data)
     return _GetItemUnitxtID(data[1], data[2], data[3])
+end
+
+-- Full item data lookup
+local function _GetWeaponData(group, index, itemdata)
+    local weaponAddress = pso.read_u32(pmtAddress + pmtWeaponOffset)
+    if weaponAddress ~= 0 then
+        local groupAddress = weaponAddress + 8 * group
+
+        local count = pso.read_u32(groupAddress)
+        local itemAddress = pso.read_u32(groupAddress + 4)
+
+        if index < count and itemAddress ~= 0 then
+            -- id = pso.read_i32(itemAddress + 44 * index)
+        end
+    end
+end
+local function _GetArmorData(group, index, itemdata)
+    itemdata.armor = {}
+    itemdata.armor.dfp = 0
+    itemdata.armor.evp = 0
+    itemdata.armor.blockParticle = 0
+    itemdata.armor.blockEffect = 26
+    itemdata.armor._class = 0
+    itemdata.armor.level = 1
+    itemdata.armor.efr = 0
+    itemdata.armor.eth = 0
+    itemdata.armor.eic = 0
+    itemdata.armor.edk = 0
+    itemdata.armor.elt = 0
+    itemdata.armor.dfpR = 0
+    itemdata.armor.evpR = 0
+    itemdata.armor.statBoost = 0
+    itemdata.armor.techBoost = 0
+    itemdata.armor.unknown1 = 0
+    itemdata.armor.unknown2 = 0
+
+    local armorAddress = pso.read_u32(pmtAddress + pmtArmorOffset)
+    if armorAddress ~= 0 then
+        local groupAddress = armorAddress + 8 * (group - 1)
+
+        local count = pso.read_u32(groupAddress)
+        local itemAddress = pso.read_u32(groupAddress + 4)
+
+        if index < count and itemAddress ~= 0 then
+            itemdata.id =                   pso.read_u32(itemAddress + 32 * index + 0)
+            itemdata.model =                pso.read_u16(itemAddress + 32 * index + 4)
+            itemdata.texture =              pso.read_u16(itemAddress + 32 * index + 6)
+            itemdata.teampoints =           pso.read_u32(itemAddress + 32 * index + 8)
+            itemdata.armor.dfp =            pso.read_u16(itemAddress + 32 * index + 12)
+            itemdata.armor.evp =            pso.read_u16(itemAddress + 32 * index + 14)
+            itemdata.armor.blockParticle =  pso.read_u8(itemAddress + 32 * index + 16)
+            itemdata.armor.blockEffect =    pso.read_u8(itemAddress + 32 * index + 17)
+            itemdata.armor._class =         pso.read_u16(itemAddress + 32 * index + 18)
+            itemdata.armor.level =          pso.read_u8(itemAddress + 32 * index + 20) + 1
+            itemdata.armor.efr =            pso.read_i8(itemAddress + 32 * index + 21)
+            itemdata.armor.eth =            pso.read_i8(itemAddress + 32 * index + 22)
+            itemdata.armor.eic =            pso.read_i8(itemAddress + 32 * index + 23)
+            itemdata.armor.edk =            pso.read_i8(itemAddress + 32 * index + 24)
+            itemdata.armor.elt =            pso.read_i8(itemAddress + 32 * index + 25)
+            itemdata.armor.dfpR =           pso.read_u8(itemAddress + 32 * index + 26)
+            itemdata.armor.evpR =           pso.read_u8(itemAddress + 32 * index + 27)
+            itemdata.armor.statBoost =      pso.read_u8(itemAddress + 32 * index + 28)
+            itemdata.armor.techBoost =      pso.read_u8(itemAddress + 32 * index + 29)
+            itemdata.armor.unknown1 =       pso.read_u8(itemAddress + 32 * index + 30)
+            itemdata.armor.unknown2 =       pso.read_u8(itemAddress + 32 * index + 31)
+        end
+    end
+end
+local function _GetUnitData(index, itemdata)
+    local unitAddress = pso.read_u32(pmtAddress + pmtUnitOffset)
+
+    local count = pso.read_u32(unitAddress)
+    local itemAddress = pso.read_u32(unitAddress + 4)
+
+    if index < count and itemAddress ~= 0 then
+        --id = pso.read_i32(itemAddress + 20 * index)
+    end
+end
+local function _GetMagData(index, itemdata)
+    local magAddress = pso.read_u32(pmtAddress + pmtMagOffset)
+
+    local count = pso.read_u32(magAddress)
+    local itemAddress = pso.read_u32(magAddress + 4)
+
+    if group < count and itemAddress ~= 0 then
+        --id = pso.read_i32(itemAddress + 28 * group)
+    end
+end
+local function _GetToolData(group, index, itemdata)
+    local toolAddress = pso.read_u32(pmtAddress + pmtToolOffset)
+    if toolAddress ~= 0 then
+        local groupAddress = toolAddress + 8 * group
+
+        local count = pso.read_u32(groupAddress)
+        local itemAddress = pso.read_u32(groupAddress + 4)
+
+        if index < count and itemAddress ~= 0 then
+            id = pso.read_i32(itemAddress + 24 * index)
+        end
+    end
+end
+
+local function _GetItemData(type, group, index)
+    local itemdata = {}
+    itemdata.id = -1
+    itemdata.model = -1
+    itemdata.texture = -1
+    itemdata.teampoints = 0
+
+    if LoadPMTAddress() ~= 0 then
+        if type == 0 then
+            itemdata = _GetWeaponData(group, index, itemdata)
+        elseif type == 1 then
+            if group == 1 or group == 2 then
+                itemdata = _GetArmorData(group, index, itemdata)
+            elseif group == 3 then
+                itemdata = _GetUnitData(index, itemdata)
+            end
+        elseif type == 2 then
+            itemdata = _GetMagData(index, itemdata)
+        elseif type == 3 then
+            itemdata = _GetToolData(group, index, itemdata)
+        end
+    end
+end
+local function GetItemData(data)
+    return _GetItemData(data[1], data[2], data[3])
 end
 
 return
 {
     GetItemUnitxtID = GetItemUnitxtID,
+    GetItemData = GetItemData,
 }
