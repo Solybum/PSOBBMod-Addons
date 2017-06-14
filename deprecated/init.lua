9c0485bfd9c3d1fbbe83645522723d5fc0a2498c
Soleil Rojas <soleilrojas@outlook.com>
2017-06-14 07:09:15 -0600

    Moved deprecated addons into ./deprecated/ If you want to keep using them, you have to manually copy them into ./addons/ in your game directory.

---
+++ deprecated/init.lua
@@ -0,0 +1,18 @@
+local function init()
+    return
+    {
+        name = "Old Addons",
+        version = "1.0.0",
+        author = "Solybum",
+        description = "Deprecated init so the thing doesn't crash",
+        present = nil,
+    }
+end
+
+return
+{
+    __addon =
+    {
+        init = init
+    }
+}
